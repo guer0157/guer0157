5c3e6e8a1146ccab8b38cf1232dffa7c40671625
guer0157 <32390693+guer0157@users.noreply.github.com>
2017-10-15 03:31:14 -0400

    Changed scope of event listener functions

--- namespace.js
+++ namespace.js
@@ -1,11 +1,10 @@
-
 var GUER0157 = {
 
     init: function () {
 
         // Append The Div
-        var box = document.createElement('div'),  
-        boxes = document.getElementById('boxes');
+        var box = document.createElement('div'),
+            boxes = document.getElementById('boxes');
 
         // Add Text
         box.textContent = "GUER0157";
@@ -18,21 +17,18 @@ var GUER0157 = {
 
         // Add Event Listeners
         box.addEventListener('mouseover', mouseOverOut);
-        box.addEventListener('mouseout', GUER0157.mouseOverOut);
-        box.addEventListener('click', GUER0157.click);
-
-        //var box = document.querySelector(".box");
+        box.addEventListener('mouseout', mouseOverOut);
+        box.addEventListener('click', mouseClick);
         
-    }
+        function mouseOverOut (event) {        
+            event.currentTarget.classList.toggle('highlight');
+        }
 
-    function mouseOverOut ( event ) {        
-        event.currentTarget.classList.toggle('highlight');
-    }
+        function mouseClick (event) {
+            event.currentTarget.style.borderColor = "#00FF00";
+            event.currentTarget.style.backgroundColor = "#FF0000";
+        }
 
-
-    function click ( event ) {
-        event.currentTarget.style.borderColor = "#00FF00";
-        event.currentTarget.style.backgroundColor = "#FF0000";
     }
 
 };
