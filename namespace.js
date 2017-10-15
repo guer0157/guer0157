b54109947658af8c872ff12c997e51b696b591b4
guer0157 <32390693+guer0157@users.noreply.github.com>
2017-10-15 03:01:58 -0400

    namespace assignment
    name space assignment

---
+++ namespace.js
@@ -0,0 +1,39 @@
+
+var GUER0157 = {
+
+    function init () {
+
+        // Append The Div
+        var box = document.createElement('div');
+
+        // Add Text
+        box.textContent = "GUER0157";
+
+        // Add Class to box
+        box.classList.add('box');
+
+        // Append the box to #boxes
+        var boxes = document.getElementById("boxes");
+        boxes.appendChild(box);
+
+        // Add Event Listeners
+        box.addEventListener('mouseover', mouseOverOut);
+        box.addEventListener('mouseout', mouseOverOut);
+        box.addEventListener('click', click);
+
+        //var box = document.querySelector(".box");
+        
+    }
+
+    function mouseOverOut ( event ) {        
+        event.currentTarget.classList.toggle('highlight');
+    }
+
+
+    function click ( event ) {
+        event.currentTarget.style.borderColor = "#00FF00";
+        event.currentTarget.style.backgroundColor = "#FF0000";
+    }
+
+}
+
